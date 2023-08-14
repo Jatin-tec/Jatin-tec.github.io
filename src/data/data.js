2db7b000707daa10e7c235ed76fa5d9770a2b8db
jatin-tec <jatin.kshatriya2821@gmail.com>
2023-08-15 00:47:42 +0530

    updating projects

--- src/data/data.js
+++ src/data/data.js
@@ -4,25 +4,34 @@ export const data = [
   {
     id: 1,
     img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
-    title: "Multimedia Web",
-    desc: "",
-    link: "",
-    git: "",
+    title: "SkinZen",
+    desc: "SkinZen is a web-based product that accurately detects and classifies the severity of acne using the ResNet-101 architecture. The model was pre-trained with weights from the ImageNet dataset. The project incorporated additional models like eye-cascade and face-landmarks to extract skin patches from training images, enhancing the classification process.",
+    link: "https://huggingface.co/spaces/jatin-tech/SkinZen",
+    git: "https://github.com/WebCrisp/SkinZen",
   },
   {
     id: 2,
     img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
-    title: "Tarvel Agency",
+    title: "DataSeekr",
+    desc: "DataSeekr is a powerful search engine application designed to help you efficiently search and explore your personal data, specifically focusing on Gmail.",
     link: "",
-    git: "",
+    git: "https://github.com/Jatin-tec/DataSeekr",
   },
   {
     id: 3,
     img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
-    title: "Music Player",
-    desc: "",
+    title: "Genetic Algorithm",
+    desc: "This project demonstrates a genetic algorithm that evolves a population of rockets to reach a target while avoiding an obstacle.",
     link: "",
     git: "",
-  }
+  },
+  {
+    id: 4,
+    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
+    title: "Video Action Classification",
+    desc: "",
+    link: "https://colab.research.google.com/drive/1paSC1fDK5G8sMYAjG-yyuVpXGUJX06vi?usp=sharing",
+    git: "https://github.com/WebCrisp/SkinZen",
+  },
   
 ];
